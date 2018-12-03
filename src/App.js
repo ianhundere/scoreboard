b70da457d8820feeaa6ac46e056fe0a43a5d743c
Ian Hundere <ianfundere@gmail.com>
2018-12-03 12:55:03 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,126 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    // set up state
+    this.state = {
+      scores: [
+        {
+          id: 1,
+          name: "c.c.",
+          score: 1001
+        },
+        {
+          id: 2,
+          name: "mel",
+          score: 10
+        },
+        {
+          id: 3,
+          name: "sid",
+          score: 800
+        }
+      ]
+    }
+
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {''}
+        <h1>Scoreboard App</h1>
+        <div className="score-container">
+          {this._scoresAsCards()}
+        </div>
       </div>
     );
   }
-}
 
+  _scoresAsCards() {
+    const cards = this.state.scores.map(score => {
+      return (
+        <div key={score.id}>
+          <h2>
+            Name: {score.name} // Score: {score.score}
+            </h2>
+            <button onClick={() => this._incrementScoreById(score.id)}>+</button>
+        </div>
+      );
+    });
+    return cards;
+  }
+  // vers 1: .map, manually constructing replacement
+  // _incrementScoreById(id) {
+  //   // console.log("Wee hee hee!")
+  //   // find the player in this.state.scores
+  //   // increment their score
+  //   const newScores = this.state.scores.map(score => {
+  //     if (score.id !== id)
+  //     return score
+  //     else  {
+  //       return {
+  //         id: score.id,
+  //         name: score.name,
+  //         score: score.score + 1
+  //       }
+  //     }
+  //   })
+  //   // and call this.setState
+  //   this.setState({
+  //     scores: newScores
+  //   })
+  // }
+  // vers 2: .map, using a shorthand to copy values out of the original
+  // _incrementScoreById(id) {
+    // find the player in this.state.scores
+    // increment their score
+    // const newScores = this.state.scores.map(score => {
+    //   if(score.id !== id) {
+    //     return score;
+    // } else {
+    //   return {
+    //     ...score,
+    //     score: score.score +1
+    //   };
+    // }
+    // });
+    // and call this.setState
+//   this.setState({
+//     scores: newScores
+//   })
+
+// }
+
+  // vers 3: .map, object copy + ternary + implicit return
+  // using a shorthand to copy values out of the original score
+  // _incrementScoreById(id) {
+    // find the player in this.state.score
+    // increment their score
+    // const newScores = this.state.scores.map(data => {
+    //   return data.id !== id ? data : { ...data, score: data.score + 1};
+    // });
+    // and calling this.setState
+  //   this.setState({
+  //     scores: newScores
+  //   });
+  // }
+
+  //Version 0:
+  _incrementScoreById(id) {
+    // find the player in this.state.score and increment their score
+    const newScores = this.state.scores.map(score => {
+      // change the score for the matching id
+      if (score.id === id) {
+        score.score += 1;
+      }
+      return score;
+    });
+
+    this.setState({
+      scores: newScores
+    })
+  }
+}
 export default App;
